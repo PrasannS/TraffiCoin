dc37397b55fc5fa5e4d6c4193638d28d4933fa26
PrasannS <singhal.prasann07@gmail.com>
2019-05-19 02:27:58 -0500

    Hourly traffic logic set up.

--- app/src/main/java/cryptonite/android/apps/com/traficoin/BackgroundService.java
+++ app/src/main/java/cryptonite/android/apps/com/traficoin/BackgroundService.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 import org.greenrobot.greendao.query.QueryBuilder;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,6 +19,11 @@ import cryptonite.android.apps.com.traficoin.Models.Day;
 import cryptonite.android.apps.com.traficoin.Models.Route;
 import cryptonite.android.apps.com.traficoin.Models.RouteDao;
 import cryptonite.android.apps.com.traficoin.Models.Traffic;
+import cryptonite.android.apps.com.traficoin.TrafficSDK.CF;
+import cryptonite.android.apps.com.traficoin.TrafficSDK.FI;
+import cryptonite.android.apps.com.traficoin.TrafficSDK.FI_;
+import cryptonite.android.apps.com.traficoin.TrafficSDK.RW;
+import cryptonite.android.apps.com.traficoin.TrafficSDK.RW_;
 import cryptonite.android.apps.com.traficoin.TrafficSDK.RetrofitClient;
 import cryptonite.android.apps.com.traficoin.TrafficSDK.TrafficResponse;
 
@@ -25,6 +31,9 @@ public class BackgroundService extends Service implements RetrofitClient.Traffic
     public BackgroundService() {
     }
 
+    public List<Traffic>traffics = new ArrayList<>();
+    Traffic cur = new Traffic();
+    public int count=0;
     public DaoSession daoSession;
     @Override
     public void onStart(Intent intent, int startId) {
@@ -34,20 +43,38 @@ public class BackgroundService extends Service implements RetrofitClient.Traffic
         Timer t = new Timer();
 //Set the schedule function and rate
         Route tempa = getRouteFromPriority();
+        final int starthrs = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        final int startmins = Calendar.getInstance().get(Calendar.MINUTE);
         while (tempa!=null){
             final Route temp = tempa;
-            List<Traffic> traffics = new ArrayList<>();
             t.scheduleAtFixedRate(new TimerTask() {
                                       @Override
                                       public void run() {
                                           //Called each time when 1000 milliseconds (1 second) (the period parameter)
                                           RetrofitClient retrofitClient = new RetrofitClient(BackgroundService.this);
                                           retrofitClient.getResponse(temp.getStartlat(), temp.getStartlng(), temp.getEndlat(), temp.getEndlng());
-
+                                          cur.setEndhours(Calendar.HOUR_OF_DAY);
+                                          cur.setEndmins(Calendar.MINUTE);
+                                          cur.setStarthours(starthrs);
+                                          cur.setStartmins(startmins);
+                                          cur.setEndlat(temp.getEndlat());
+                                          cur.setEndlng(temp.getEndlng());
+                                          cur.setStartlat(temp.getStartlat());
+                                          cur.setStartlng(temp.getStartlng());
+                                          cur.setRouteID(getRouteKey(temp.getEndlat(),temp.getEndlng(),temp.getStartlat(),temp.getStartlng()));
+                                          daoSession.getTrafficDao().insertInTx(cur);
+                                          count++;
+                                          if(count==49){
+                                              count=0;
+                                              temp.setPending(false);
+                                              daoSession.update(temp);
+                                          }
+                                          final int starthrs = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+                                          final int startmins = Calendar.getInstance().get(Calendar.MINUTE);
                                       }
                                   },
 //Set how long before to start calling the TimerTask (in milliseconds)
-                    0,
+                    18000000,
 //Set the amount of time between each execution (in milliseconds)
                     18000000);
         }
@@ -73,7 +100,42 @@ public class BackgroundService extends Service implements RetrofitClient.Traffic
 
     @Override
     public void onTrafficResponse(TrafficResponse tr) {
+        List<RW> rws = tr.getRWS();
+        double d=0;
+        int z=0;
+        for(RW r:rws){
+            List<RW_> rs = r.getRW();
+            for(RW_ a:rs){
+                List<FI> fis = a.getFIS();
+                for(FI f:fis){
+                    List<FI_> fs = f.getFI();
+                    for(FI_ g:fs){
+                        List<CF>cs = g.getCF();
+                        for(CF c:cs){
+                            d+=c.getJF();
+                            z++;
+                        }
+                    }
+                }
+            }
+        }
+        d/=z;
+        cur.setJf(d);
 
+    }
+
+    public String getRouteKey(double elat, double elng,double slat, double slng){
+        List<Route> routes = daoSession.getRouteDao().loadAll();
+        for(Route r: routes){
+            double a = r.getEndlat()-elat;
+            double c = r.getStartlat()-slat;
+            double b = r.getStartlng()-slng;
+            double d = r.getEndlng()-elng;
+            if(CoinGeneratorClient.getDistanceKm(c,a,b,d)<.3){
+                return r.getRouteID();
+            }
+        }
+        return null;
     }
 
     public ArrayList<Traffic>getRushHours(ArrayList<Traffic>traffics){
