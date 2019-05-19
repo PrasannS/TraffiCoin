3fe507c312189e6357e442ae7d9049ac5af6b34e
lucas.z.cai <lucas.z.cai@gmail.com>
2019-05-18 20:04:38 -0500

    some UI stuff, i kinda lost track lmao

---
+++ app/src/main/java/cryptonite/android/apps/com/traficoin/GoalSetActivity.java
@@ -0,0 +1,13 @@
+package cryptonite.android.apps.com.traficoin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoalSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_set);
+    }
+}
--- app/src/main/java/cryptonite/android/apps/com/traficoin/LoginActivity.java
+++ app/src/main/java/cryptonite/android/apps/com/traficoin/LoginActivity.java
@@ -1,13 +1,26 @@
 package cryptonite.android.apps.com.traficoin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signIn = (Button)findViewById(R.id.signInBtn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
