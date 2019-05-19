da4a9807580acc66593aa48dde634fce5b023132
YuanSamuel <samuel6161@gmail.com>
2019-05-19 03:15:53 -0500

    fire welcome screen

--- app/src/main/java/cryptonite/android/apps/com/traficoin/LoginActivity.java
+++ app/src/main/java/cryptonite/android/apps/com/traficoin/LoginActivity.java
@@ -5,19 +5,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends Activity {
-    TextView t, test;
-    public static final String TAG = LoginActivity.class.getSimpleName();
-    LocationTracker lc;
+
+    TextView welcome;
     Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
-        lc = new LocationTracker(this,getApplication());
+        welcome = (TextView) findViewById(R.id.welcomeTxt);
+        Animation in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(3000);
+        welcome.startAnimation(in);
         signIn = (Button)findViewById(R.id.signInBtn);
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,14 +31,6 @@ public class LoginActivity extends Activity {
                 startActivity(i);
             }
         });
-    }
-    // register the RX and start up the ActivityDetectionService service
-    @Override
-    protected void onStart() {
-        super.onStart();
-        startService(new Intent(this, ActivityDetectionService.class));
-        startService(new Intent(getBaseContext(), BackgroundService.class));
-        lc.start(this);
     }
 
 }
